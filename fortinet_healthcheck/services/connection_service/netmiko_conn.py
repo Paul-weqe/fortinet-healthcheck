2219cadadbbf4cb92293da8731d8c3dc6593158e
Brunner Toywa <brunnerkibali@gmail.com>
2022-08-21 13:23:29 +0300

    Adding OOP comments

--- fortinet_healthcheck/services/connection_service/netmiko_conn.py
+++ fortinet_healthcheck/services/connection_service/netmiko_conn.py
@@ -1,20 +1,24 @@
 from netmiko import Netmiko
 
-
+# Defining the FortinetException object
 class FortinetException(Exception):
+    
     pass
 
 
 def create_connection(hostname: str, username: str, password: str, device_type: str = 'fortinet'):
+
     conn_details = {
         'device_type': device_type,
         'host': hostname, 
         'username': username, 
         'password': password
     }
+
     try:
         conn = Netmiko(**conn_details)
         return conn
+
     except Exception as e:
         raise FortinetException(f"Unable to connect to host: {hostname}\nERROR: \n {e}")
 
